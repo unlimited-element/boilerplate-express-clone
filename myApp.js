e27cf1d21cc7c091c1f1e6098608a6bdb5bbc7ba
unlimited-element <73407996+unlimited-element@users.noreply.github.com>
2021-05-14 21:09:52 -0700

    Served html as root file

--- myApp.js
+++ myApp.js
@@ -1,20 +1,10 @@
 var express = require('express');
 var app = express();
 
+
 app.get("/", function(req, res) {
-  res.send('Hello Express');
+  res.sendFile(__dirname + '/views/index.html');
 });
-
-console.log("Hello World")
-
-
-
-
-
-
-
-
-
 
 
 
